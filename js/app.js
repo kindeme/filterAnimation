ac9ab4e073d9124e42ca243c23c503366e3c9186
Kindeme <fkounoudji@gmail.com>
2020-01-22 11:41:23 -0600

    filter js

---
+++ js/app.js
@@ -0,0 +1,69 @@
+// filter btns
+(function(){
+    //select all buttons
+
+    const filterBtn = document.querySelectorAll(".filter-btn");
+    //console.log(filterBtn);
+    
+    filterBtn.forEach(function(btn){
+ btn.addEventListener("click",function(event){
+     //prevemt default action
+     event.preventDefault();
+     const value = event.target.dataset.filter;
+     //console.log(value);
+     const items = document.querySelectorAll(".store-item");
+     //console.log(items);
+     items.forEach(function(item){
+         if(value === 'all'){
+             item.style.display = "block";
+         }
+         else{
+          if (item.classList.contains(value))
+         {
+            item.style.display = "block";
+         }else{
+            item.style.display = "none";
+         }
+        }
+     });
+
+ });
+    });
+
+})();
+
+
+// search input
+(function(){
+    //target search box
+    const search = document.getElementById("search-item");
+    search.addEventListener('keyup',function(){
+        let value = search.value.toLowerCase().trim();
+        //console.log(value);
+        const items = document.querySelectorAll(".store-item");
+        //cnsole.log(value);
+        items.forEach(function(item){
+            let type = item.dataset.item;
+            //console.log(type);
+
+            //search with includes
+            // if(type.includes(value))
+            // {
+            //     item.style.display = "block";
+            // } else{
+            //     item.style.display = "none";
+            // }
+ let length = value.length;
+ let match = type.slice(0,length);
+if(value == match){
+    item.style.display = "block";
+
+}else{
+    item.style.display = "none";
+}
+
+        });
+
+    });
+
+})();~
